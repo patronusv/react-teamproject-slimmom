c3dd61ac7d987ebca2ca9f784baea8dfdaf3e023
Vera Korzun <veraminich@gmail.com>
2021-02-15 21:42:02 +0100

    Added component Loader

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,5 +1,12 @@
-const Loader = () => {
-  return <></>;
+import React from 'react';
+import Loader from 'react-loader-spinner';
+
+const LoaderSpinner = () => {
+  return (
+    <div className="loader">
+      <Loader type="ThreeDots" color="#FC842D" height={80} width={80} />
+    </div>
+  );
 };
 
-export default Loader;
+export default LoaderSpinner;
---
+++ src/components/loader/LoaderStyled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  .loader {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.4);
+  }
+`;
+
+export default Wrapper;
