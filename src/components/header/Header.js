01acddf9a14ec1c4e285b2db91510a68bce9a1b5
TanyaChichkan <tchichkan28@gmail.com>
2021-02-15 22:57:00 +0200

    first steps

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,27 @@
+import Logo from '../logo/Logo';
+import {HeaderContainer,Nav,Container} from './headerStyled';
+
 const Header = () => {
-  return <></>;
+
+  return (
+    <>
+      <HeaderContainer>
+        <Container className="container">
+          <Logo/>
+          <Nav>
+            <ul className="nav-list">
+              <li className="nav-list-item">
+                Enter
+              </li>
+              <li className="nav-list-item">
+                Registration
+              </li>
+            </ul>
+          </Nav>
+        </Container>
+      </HeaderContainer>
+    </>
+  );
 };
 
 export default Header;
