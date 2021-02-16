5e888453401a9a933e3531442d7e2a26d2b732a9
TanyaChichkan <tchichkan28@gmail.com>
2021-02-16 09:06:03 +0200

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 import Header from './header/Header';
 import Main from './main/Main';
+import {Suspense} from 'react';
+import { Switch, Route, NavLink } from "react-router-dom";
+import mainRoutes from '../routes/mainRoutes';
+
 
 const App = () => {
   return (
     <>
       <Header />
       <Main />
+      {/* <Suspense fallback={<h2>...loading</h2>}>
+        <Switch>
+        {mainRoutes.map((route) => (
+            route.isPrivate ? (
+            <PrivateRoute {...route} isAuth={isAuth} key={route.path} />
+          ) : (
+            <PublicRoute {...route} isAuth={isAuth} key={route.path} />
+          )
+        ))}
+        </Switch>
+      </Suspense> */}
     </>
   );
 };
