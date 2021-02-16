dda7d01bcf96c5bd80c1693316b44eaa25c5b307
Vera Korzun <veraminich@gmail.com>
2021-02-16 18:08:38 +0100

    Added changes

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Loader from 'react-loader-spinner';
+import LoaderSpinnerStyled from './LoaderStyled';
 
 const LoaderSpinner = () => {
   return (
-    <div className="loader">
-      <Loader type="ThreeDots" color="#FC842D" height={80} width={80} />
-    </div>
+    <LoaderSpinnerStyled>
+      <div className="loader">
+        <Loader type="ThreeDots" color="#FC842D" height={80} width={80} />
+      </div>
+    </LoaderSpinnerStyled>
   );
 };
 
--- src/components/loader/LoaderStyled.js
+++ src/components/loader/LoaderStyled.js
@@ -6,8 +6,8 @@ const Wrapper = styled.div`
     justify-content: center;
     align-items: center;
     position: absolute;
-    top: 0;
-    left: 0;
+    /* top: 0;
+    left: 0; */
     width: 100%;
     height: 100%;
     background-color: rgba(0, 0, 0, 0.4);
