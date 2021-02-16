e0240432298ed8f24011001b7fb25d10d7a03ebe
Valentina <dert12345f@ukr.net>
2021-02-16 14:41:13 +0200

    styles forms login and registration

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,6 @@
 import Header from './header/Header';
 import Main from './main/Main';
 
-import LoginForm from './loginForm/LoginForm';
-import RegistrationForm from './registrationForm/RegistrationForm';
-
 const App = () => {
   return (
     <>
