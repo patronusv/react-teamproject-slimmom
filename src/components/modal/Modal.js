4ddbf48ea06740375fd923d01d062f68be06984c
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-02-16 19:08:19 +0200

    modal

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,26 +1,62 @@
-import { Component, useState } from 'react';
-import './Modal.css';
-import IconClose from './iconClose/IconClose';
+import React, { useEffect } from 'react';
+import { useWindowWidth } from '@react-hook/window-size';
+import style from './Modal.module.css';
 
-const Modal = props => {
+const Modal = ({ children, openModal, toggleModal }) => {
+  //   у компоненті, який викликає модальне вікно, потрібно створити локальний стейт:
+  //   const [openModal, setOpenModal]= useState(false)
+  //   та прописати логіку тогла (закриття/відкриття модального вікна):
+  //   const toggleModal=()=>{
+  //    setOpenModal(!openModal)
+  //  }
+  //  та передати їх пропсами
+
+  const onlyWidth = useWindowWidth();
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    document.body.className = openModal ? style.open : style.close;
+    //  document.body.style.overflow=openModal?"hidden":"visible";
+  }, [openModal]);
+
+  const handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      toggleModal();
+    }
+  };
+  const handleClick = e => {
+    if (e.target.dataset.name !== 'overlay') {
+      return;
+    }
+    toggleModal();
+  };
+
   return (
     <>
-      <button onClick="">OpenModal</button>
-
-      <div
-        className={`modal__wrapper ${props.isOpened ? 'open' : 'close'}`}
-        style={{ ...props.style }}
-      >
-        <div className="modal__body">
-          <button type="button">
-            <IconClose />
-          </button>
-
-          <h2>Ваша рекомендуемая суточная норма калорий составляет</h2>
-          <hr />
-          {props.children}
+      {openModal && (
+        <div
+          className={style.overlay}
+          onClick={handleClick}
+          data-name="overlay"
+        >
+          <div className={style.modal} data-name="modal">
+            <button
+              className={
+                onlyWidth < 768 ? style.arrowCloseButton : style.closeButton
+              }
+              type="button"
+              onClick={toggleModal}
+            ></button>
+            {children}
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
