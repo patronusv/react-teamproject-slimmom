6ca22e1419618367c727bd85ae7f1e60633925a2
Viktor Gorokhov <patronusv@gmail.com>
2021-02-18 14:52:29 +0200

    adds error reducer

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,7 +1,7 @@
-import {Suspense} from 'react';
-import {useSelector} from 'react-redux';
-import {isAuth} from '../../redux/selectors/authSelectors';
-import { Switch, Route, NavLink } from "react-router-dom";
+import { Suspense } from 'react';
+import { useSelector } from 'react-redux';
+import { isAuth } from '../../redux/selectors/authSelectors';
+import { Switch, Route, NavLink } from 'react-router-dom';
 import mainRoutes from '../../routes/mainRoutes';
 import PublicRoute from '../publicRoute/PublicRoute';
 import GeneralRoute from '../publicRoute/GeneralRoute';
@@ -9,39 +9,41 @@ import PrivateRoute from '../privateRoute/PrivateRoute';
 import Home from '../../pages/home/Home';
 
 const Main = () => {
-
-  const authFlag  = useSelector(isAuth);
+  const authFlag = useSelector(isAuth);
   // const authFlag = true;
   const dailyRate = useSelector(state => state.health.dailyRate);
 
   return (
     <>
-     <Suspense fallback={<h2>...loading</h2>}>
-    
+      <Suspense fallback={<h2>...loading</h2>}>
         <Switch>
-          
-        
-        {mainRoutes.map((route) => {
-            if (route.isPrivate===true){
-            return dailyRate && (<PrivateRoute {...route} isAuth={authFlag} key={route.path}/>) 
-      
-            } 
-            if(route.isPrivate===false){
-              return (<PublicRoute {...route} isAuth={authFlag} key={route.path} dailyRate={dailyRate}/>)
+          {mainRoutes.map(route => {
+            if (route.isPrivate === true) {
+              return (
+                dailyRate.dailyRate && (
+                  <PrivateRoute {...route} isAuth={authFlag} key={route.path} />
+                )
+              );
             }
-        })}
+            if (route.isPrivate === false) {
+              return (
+                <PublicRoute
+                  {...route}
+                  isAuth={authFlag}
+                  key={route.path}
+                  dailyRate={dailyRate}
+                />
+              );
+            }
+          })}
 
-        <Route path='/' exact={true} component={Home} />
+          <Route path="/" exact={true} component={Home} />
 
-       
-
-          <GeneralRoute/>
+          <GeneralRoute />
         </Switch>
-
-      
       </Suspense>
     </>
-    );
+  );
 };
 
 export default Main;
--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import authReducer from './authReducer';
+import errorReducer from './errorReducer';
 import healthReducer from './healthReducer';
 import loaderReducer from './loaderReducer';
 
@@ -15,6 +16,7 @@ const rootReducer = combineReducers({
   auth: persistReducer(authPersistConfig, authReducer),
   health: healthReducer,
   loading: loaderReducer,
+  error: errorReducer,
 });
 
 export default rootReducer;
