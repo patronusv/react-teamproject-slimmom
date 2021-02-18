f7a123553962554450b2604fe43b7c58a9bda48a
Viktor Gorokhov <patronusv@gmail.com>
2021-02-18 16:08:55 +0200

    removed useffect in diarylistitem

--- src/components/diaryProductList/DiaryProductList.js
+++ src/components/diaryProductList/DiaryProductList.js
@@ -1,48 +1,44 @@
-import React, {useEffect} from 'react';
-import { useDispatch, useSelector} from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import DiaryProductItem from './diaryProductItem/DiaryProductItem';
 import healthOperations from '../../redux/operations/healthOperations';
 import healthSelectors from '../../redux/selectors/healthSelectors';
-import { UL,MaskWrapper,ListWrapper} from './DiaryProductListStyle'
-
+import { UL, MaskWrapper, ListWrapper } from './DiaryProductListStyle';
 
 const DiaryProductList = () => {
   const dispatch = useDispatch();
   const eatenProdArray = useSelector(healthSelectors.getEatenProducts);
 
-  useEffect(() => {
-    healthOperations.getDayInfoOperation();
-  }, [])
- 
- 
-  const handleDelete =(e)=>{
+  const handleDelete = e => {
     const prodID = e.target.dataset.id;
     console.log('prodID', prodID);
     dispatch(healthOperations.deleteDiaryItemOperation(prodID));
-  }
+  };
 
   return (
     <div className="container">
-      {eatenProdArray  ?
-        (<ListWrapper>
+      {eatenProdArray ? (
+        <ListWrapper>
           <UL id="element">
-              {eatenProdArray.length >0 ? eatenProdArray.map(item=>
-                (<DiaryProductItem onClick = {handleDelete} {...item} key={item.id}/>)) :
-                (<h2>Ваш список продуктов пуст</h2>)
-              }
+            {eatenProdArray.length > 0 ? (
+              eatenProdArray.map(item => (
+                <DiaryProductItem
+                  onClick={handleDelete}
+                  {...item}
+                  key={item.id}
+                />
+              ))
+            ) : (
+              <h2>Ваш список продуктов пуст</h2>
+            )}
           </UL>
           <MaskWrapper></MaskWrapper>
-          </ListWrapper>) : (
+        </ListWrapper>
+      ) : (
         <p>Пожалуйста, получите информацию по текущему дню</p>
-        )}
+      )}
     </div>
-    
-  )
-}
-
-
+  );
+};
 
 export default DiaryProductList;
-
-
-
