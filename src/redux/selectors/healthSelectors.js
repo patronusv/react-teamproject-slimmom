ccb919c1293b7d67f9c117ddeea6186213174936
Nadezhda Chornaya <nadezhdachornaja@gmail.com>
2021-02-18 17:06:01 +0200

    added selectors date and products

--- src/redux/selectors/healthSelectors.js
+++ src/redux/selectors/healthSelectors.js
@@ -3,21 +3,15 @@ const getEatenProducts = state => state.health.dayInfo.eatenProducts;
 const getNotAllowedProducts = state =>
   state.health.dailyRate.notAllowedProducts;
 const getDailyRate = state => state.health.dailyRate;
-<<<<<<< HEAD
 
 const getDate = state => state.health.getDate.date;
 const getProducts = state => state.health.product;
-=======
->>>>>>> dev
 
 export default {
   getDaySummary,
   getEatenProducts,
   getNotAllowedProducts,
   getDailyRate,
-<<<<<<< HEAD
   getDate,
   getProducts
-=======
->>>>>>> dev
 };
