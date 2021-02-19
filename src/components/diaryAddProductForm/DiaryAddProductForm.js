ebdd43e23bdbe1833dcb1265a30c3bbf1a970175
Viktor Gorokhov <patronusv@gmail.com>
2021-02-20 01:04:52 +0200

    fixed date in dailyrate and addproductform

--- src/components/diaryAddProductForm/DiaryAddProductForm.js
+++ src/components/diaryAddProductForm/DiaryAddProductForm.js
@@ -1,18 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { debounce } from "debounce";
-import { useSelector, useDispatch } from "react-redux";
+import { debounce } from 'debounce';
+import { useSelector, useDispatch } from 'react-redux';
 import { DiaryFormWrapper } from './DiaryAddProductFormStyle';
 import healthOperations from '../../redux/operations/healthOperations';
 import modalActions from '../../redux/actions/modalActions';
 import healthSelectors from '../../redux/selectors/healthSelectors';
 
-
-
 const DiaryAddProductForm = () => {
   const debounce = require('debounce');
   const size = useWindowSize();
-  const date = useSelector((state) => healthSelectors.getDate(state))
-  const products = useSelector((state) => healthSelectors.getProducts(state))
+  const date = useSelector(healthSelectors.getDate);
+  const products = useSelector(healthSelectors.getProducts);
 
   const dispatch = useDispatch();
   const [state, setState] = useState({
@@ -20,7 +18,6 @@ const DiaryAddProductForm = () => {
     weight: '',
     productId: '',
   });
-
 
   function useWindowSize() {
     // Initialize state with undefined width/height so server and client renders match
@@ -41,84 +38,95 @@ const DiaryAddProductForm = () => {
       }
 
       // Add event listener
-      window.addEventListener("resize", handleResize);
+      window.addEventListener('resize', handleResize);
 
       // Call handler right away so state gets updated with initial window size
       handleResize();
 
       // Remove event listener on cleanup
-      return () => window.removeEventListener("resize", handleResize);
+      return () => window.removeEventListener('resize', handleResize);
     }, []); // Empty array ensures that effect is only run on mount
 
     return windowSize;
   }
 
-
-
   const handleChange = e => {
     const { name, value } = e.target;
-    setState(prev =>
-      ({ ...prev, [name]: value })
-    )
-    if (products.some((product) => product.title.ru.includes(value))) {
-      setState((prev) => ({
+    setState(prev => ({ ...prev, [name]: value }));
+    if (products.some(product => product.title.ru.includes(value))) {
+      setState(prev => ({
         ...prev,
-        productId: products.find((product) => {
-          return product.title.ru === value
-        }
-        )?._id,
+        productId: products.find(product => {
+          return product.title.ru === value;
+        })?._id,
       }));
     } else {
-      debounce(dispatch(healthOperations.getProductOperation(state.productName)), 2000)
+      debounce(
+        dispatch(healthOperations.getProductOperation(state.productName)),
+        2000,
+      );
     }
-    console.log(state)
-  }
-
-
+    console.log(state);
+  };
 
   const handleSubmit = e => {
     e.preventDefault();
-    console.log(date)
-    dispatch(healthOperations.postEatenProductOperation({ date, productId: state.productId, weight: state.weight }))
+    console.log(date);
+    dispatch(
+      healthOperations.postEatenProductOperation({
+        date,
+        productId: state.productId,
+        weight: state.weight,
+      }),
+    );
     setState({
       productName: '',
       weight: '',
       productId: '',
-    })
-    dispatch(modalActions.offModal())
-  }
-
+    });
+    dispatch(modalActions.offModal());
+  };
 
   return (
     <>
       <DiaryFormWrapper>
-        <form onSubmit={handleSubmit} className='formDairyAddProduct'>
-          <div className='inputBlockDairyAddProduct'>
-            <input type="text" list='browsers' value={state.productName} name='productName' id='fav' onChange={handleChange}
-              placeholder='Введите название продукта'
-              className='inputDairyAddProduct' />
-            <datalist id='browsers'>
-              {products.map(product => <option key={product._id} value={product.title.ru}>{product.title.ru}</option>)}
+        <form onSubmit={handleSubmit} className="formDairyAddProduct">
+          <div className="inputBlockDairyAddProduct">
+            <input
+              type="text"
+              list="browsers"
+              value={state.productName}
+              name="productName"
+              id="fav"
+              onChange={handleChange}
+              placeholder="Введите название продукта"
+              className="inputDairyAddProduct"
+            />
+            <datalist id="browsers">
+              {products.map(product => (
+                <option key={product._id} value={product.title.ru}>
+                  {product.title.ru}
+                </option>
+              ))}
             </datalist>
             <label>
-              <input type="number"
+              <input
+                type="number"
                 name="weight"
                 value={state.weight}
                 onChange={handleChange}
-                placeholder='Граммы'
-                className='inputDairyAddProduct secondInputLength' />
+                placeholder="Граммы"
+                className="inputDairyAddProduct secondInputLength"
+              />
             </label>
           </div>
-          <button type='submit' className='buttonDairyAddProduct' >
+          <button type="submit" className="buttonDairyAddProduct">
             {size.width < 768 ? 'Добавить' : '+'}
           </button>
-
         </form>
-
       </DiaryFormWrapper>
-
     </>
-  )
-}
+  );
+};
 
-export default DiaryAddProductForm
+export default DiaryAddProductForm;
--- src/components/rightSideBar/RightSideBar.js
+++ src/components/rightSideBar/RightSideBar.js
@@ -6,89 +6,124 @@ import moment from 'moment';
 
 const RightSideBar = () => {
   // const currenDate = new Date();
-  const currenDate = moment(Date.now()).format('DD.MM.YYYY');
+  // const currenDate = moment(Date.now()).format('DD.MM.YYYY');
+  const currenDate = useSelector(healthSelectors.getDate);
   const daySummary = useSelector(healthSelectors.getDaySummary);
   const notAllowedProducts = useSelector(healthSelectors.getNotAllowedProducts);
 
-  return <>
-
-    <RightSideBarStyled>
-      <div className='rightSideBar'>
-        <div className='blockLeft'>
-          <h2 className='title'>Сводка за {daySummary.date ? daySummary.date.split('-').reverse().join('.') : (currenDate)} </h2>
-          {daySummary ?
-            <ul className='list'>
-              <li className='listItem'>
-                <p className='listItemText'>Осталось
-                  <span className='listItemTextRight'>
-                    {daySummary.kcalLeft ? ((daySummary.kcalConsumed > daySummary.dailyRate ? 0 : Math.round(daySummary.kcalLeft)) + ' ккал') : '000 ккал'}
-                  </span>
-                </p>
-              </li>
-              <li className='listItem'>
-                <p className='listItemText'>Употреблено
-                  <span className='listItemTextRight'>
-                    {daySummary.kcalConsumed ? ((Math.round(daySummary.kcalConsumed)) + ' ккал') : '000 ккал'}
-                  </span>
-                </p>
-              </li>
-              <li className='listItem'>
-                <p className='listItemText'>Дневная норма
-                  <span className='listItemTextRight'>
-                    {daySummary.dailyRate ? ((Math.round(daySummary.dailyRate) + ' ккал')) : '000 ккал'}
-                  </span>
-                </p>
-              </li>
-              <li className='listItem'>
-                <p className='listItemText'>n% от нормы
-                  <span className='listItemTextRight'>
-                    {daySummary.percentsOfDailyRate ? ((Math.round(daySummary.percentsOfDailyRate)) + ' %') : '000 %'}
-                  </span>
-                </p>
-              </li>
-            </ul>
-            :
-            <ul className='list'>
-              <li className='listItem'>
-                <p className='listItemText'>Осталось
-            <span className='listItemTextRight'>000 ккал</span>
-                </p>
-              </li>
-              <li className='listItem'>
-                <p className='listItemText'>Употреблено
-            <span className='listItemTextRight'>000 ккал</span>
-                </p>
-              </li>
-              <li className='listItem'>
-                <p className='listItemText'>Дневная норма
-            <span className='listItemTextRight'>000 ккал</span>
-                </p>
-              </li>
-              <li className='listItem'>
-                <p className='listItemText'>n% от нормы
-            <span className='listItemTextRight'>000 ккал</span>
-                </p>
-              </li>
-            </ul>
-          }
+  return (
+    <>
+      <RightSideBarStyled>
+        <div className="rightSideBar">
+          <div className="blockLeft">
+            <h2 className="title">
+              Сводка за{' '}
+              {daySummary.date
+                ? daySummary.date.split('-').reverse().join('.')
+                : moment(currenDate).format('DD.MM.YYYY')}{' '}
+            </h2>
+            {daySummary ? (
+              <ul className="list">
+                <li className="listItem">
+                  <p className="listItemText">
+                    Осталось
+                    <span className="listItemTextRight">
+                      {daySummary.kcalLeft
+                        ? (daySummary.kcalConsumed > daySummary.dailyRate
+                            ? 0
+                            : Math.round(daySummary.kcalLeft)) + ' ккал'
+                        : '000 ккал'}
+                    </span>
+                  </p>
+                </li>
+                <li className="listItem">
+                  <p className="listItemText">
+                    Употреблено
+                    <span className="listItemTextRight">
+                      {daySummary.kcalConsumed
+                        ? Math.round(daySummary.kcalConsumed) + ' ккал'
+                        : '000 ккал'}
+                    </span>
+                  </p>
+                </li>
+                <li className="listItem">
+                  <p className="listItemText">
+                    Дневная норма
+                    <span className="listItemTextRight">
+                      {daySummary.dailyRate
+                        ? Math.round(daySummary.dailyRate) + ' ккал'
+                        : '000 ккал'}
+                    </span>
+                  </p>
+                </li>
+                <li className="listItem">
+                  <p className="listItemText">
+                    n% от нормы
+                    <span className="listItemTextRight">
+                      {daySummary.percentsOfDailyRate
+                        ? Math.round(daySummary.percentsOfDailyRate) + ' %'
+                        : '000 %'}
+                    </span>
+                  </p>
+                </li>
+              </ul>
+            ) : (
+              <ul className="list">
+                <li className="listItem">
+                  <p className="listItemText">
+                    Осталось
+                    <span className="listItemTextRight">000 ккал</span>
+                  </p>
+                </li>
+                <li className="listItem">
+                  <p className="listItemText">
+                    Употреблено
+                    <span className="listItemTextRight">000 ккал</span>
+                  </p>
+                </li>
+                <li className="listItem">
+                  <p className="listItemText">
+                    Дневная норма
+                    <span className="listItemTextRight">000 ккал</span>
+                  </p>
+                </li>
+                <li className="listItem">
+                  <p className="listItemText">
+                    n% от нормы
+                    <span className="listItemTextRight">000 ккал</span>
+                  </p>
+                </li>
+              </ul>
+            )}
+          </div>
+          <div className="blockRight">
+            <h2 className="title">Нерекомендуемые продукты</h2>
+            {notAllowedProducts ? (
+              <ul className="list scrollbar">
+                {notAllowedProducts ? (
+                  <li className="listItem">
+                    <p className="listItemText scrollbarText">
+                      {notAllowedProducts.slice(0, 20).join(', ')}
+                    </p>
+                  </li>
+                ) : (
+                  <li>
+                    <p>Здесь будет отображаться Ваш рацион</p>
+                  </li>
+                )}
+              </ul>
+            ) : (
+              <ul className="list">
+                <li>
+                  <p>Здесь будет отображаться Ваш рацион</p>
+                </li>
+              </ul>
+            )}
+          </div>
         </div>
-        <div className='blockRight'>
-          <h2 className='title'>Нерекомендуемые продукты</h2>
-          {notAllowedProducts ?
-            <ul className='list scrollbar'>
-              {notAllowedProducts ?
-                <li className='listItem'><p className='listItemText scrollbarText'>{notAllowedProducts.slice(0, 20).join(', ')}</p></li>
-                : <li><p>Здесь будет отображаться Ваш рацион</p></li>
-              }
-            </ul> :
-            <ul className='list'>
-              <li><p>Здесь будет отображаться Ваш рацион</p></li>
-            </ul>
-          }
-        </div>
-      </div>
-    </RightSideBarStyled>
-  </>;
+      </RightSideBarStyled>
+    </>
+  );
 };
 
 export default RightSideBar;
